194de025a86716e0624225588a408145d289aa0f
Arnaud Poncet-Montanges <ponceta@users.noreply.github.com>
2025-10-01 07:36:55 +0200

    Add type change for reach geometries
    Add type change for reach geometries

--- Pully/delta_organisations_qgep_pully.sql
+++ Pully/delta_organisations_qgep_pully.sql
@@ -400,6 +400,12 @@ ch176dc9WS007085
 
 -- Handle Mutliple geometries
 
+-- 0. Drop views
+-- To enable type change of geometry columns, views depending on these tables must be dropped
+-- You can use the script drop_view.sql in QGEP/datamodel/views folder to drop all views in the correct order
+
+-- Execute drop_view.sql (with PGADMIN or PSQL)
+
 -- 1. Find all real multiple geometries
 
 SELECT 
@@ -428,18 +434,26 @@ WHERE
 'ch176dc9WS024877',
 'ch176dc9WS024878')
 
-ALTER TABLE qgep_od.wastewater_structure ADD COLUMN detail_geometry_geometry_2 geometry(MULTIPOLYGONZ, 2056);
+-- Add new column to store original geometries
+ALTER TABLE qgep_od.wastewater_structure 
+ADD COLUMN detail_geometry_geometry_2 geometry(MULTIPOLYGONZ, 2056);
 
-UPDATE qgep_od.wastewater_structure SET detail_geometry_geometry_2 = detail_geometry_geometry
+-- Copy old geometries to new column
+UPDATE qgep_od.wastewater_structure 
+SET detail_geometry_geometry_2 = detail_geometry_geometry
 WHERE detail_geometry_geometry IS NOT NULL;
 
-UPDATE qgep_od.wastewater_structure SET detail_geometry_geometry = NULL
+-- Set to NULL geometries to avoid issues during type change
+UPDATE qgep_od.wastewater_structure 
+SET detail_geometry_geometry = NULL
 WHERE detail_geometry_geometry IS NOT NULL;
 
--- Execute drop_view.sql
 
+
+-- Set new geometry type
 ALTER TABLE qgep_od.wastewater_structure
-ALTER COLUMN detail_geometry_geometry SET DATA TYPE geometry(CURVEPOLYGONZ, 2056);
+ALTER COLUMN detail_geometry_geometry 
+SET DATA TYPE geometry(CURVEPOLYGONZ, 2056);
 
 /* -- SELECTION
 SELECT obj_id,ST_ASTEXT(ST_FORCECURVE(dump.geom)::geometry(CURVEPOLYGONZ, 2056))
@@ -458,6 +472,38 @@ FROM (
 ) selection
 WHERE ws.obj_id = selection.obj_id;
 
+/* Same treatment for reaches but with a conversion to curved geometries */
+
+-- Add new column to store original geometries
+ALTER TABLE qgep_od.reach 
+ADD COLUMN progression_geometry_2 geometry(LINESTRINGZ, 2056);
+
+-- Copy old geometries to new column
+UPDATE qgep_od.reach 
+SET progression_geometry_2 = progression_geometry
+WHERE progression_geometry IS NOT NULL;
+
+-- Remove NOT NULL constraint if exists
+ALTER TABLE qgep_od.reach
+ALTER COLUMN progression_geometry DROP NOT NULL;
+
+-- Set to NULL geometriesto avoid issues during type change
+UPDATE qgep_od.reach 
+SET progression_geometry = NULL
+WHERE progression_geometry IS NOT NULL;
+
+-- Execute drop_view.sql
+
+-- Set new geometry type
+ALTER TABLE qgep_od.reach
+ALTER COLUMN progression_geometry 
+SET DATA TYPE geometry(COMPOUNDCURVEZ, 2056);
+
+-- Fill with curved geometries
+UPDATE qgep_od.reach r
+SET progression_geometry = ST_FORCECURVE(r.progression_geometry_2)::geometry(COMPOUNDCURVEZ, 2056)
+WHERE r.progression_geometry_2 IS NOT NULL;
+
 -- Regen views (PYTHON CODE TO RUN FROM THE OSGEO4W SHELL)
 -- Check pirogue version 
 pip show pirogue
@@ -467,4 +513,3 @@ pip install -r requirements.txt
 
 -- In QGEP/datamodel run / adapt pg_service according to your connection
 python view/create_views.py -p pg_qgep_migration -s 2056
-
